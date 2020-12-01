620953058e9d8f86f4f8bd06650d9ceddac8dcc6
326th <minecraftskinman@gmail.com>
2020-12-01 12:10:53 +0700

    initial commit

---
+++ GPSextract.py
@@ -0,0 +1,93 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from decouple import config
+import requests
+import unittest
+import time
+import paho.mqtt.client as mqtt
+
+DRIVER_PATH = "C:/Users/USER/Desktop/chromedriver.exe"
+BROWSER_PATH = "C:/Program Files (x86)/BraveSoftware/Brave-Browser/Application/brave.exe"
+DELAY = 1
+
+def set_up(driver_path,browser_path):
+    """
+    set up  browser with custom driver and browser path
+    """
+
+    option = webdriver.ChromeOptions()
+    option.binary_location = browser_path
+    browser = webdriver.Chrome(executable_path=driver_path,  options=option)
+    return browser
+
+area_group = input("please enter area grouping")
+browser_host = set_up(DRIVER_PATH,BROWSER_PATH)
+browser_host.get("https://www.facebook.com/")
+time.sleep(DELAY)
+username_field = browser_host.find_element_by_name("email")
+username_field.send_keys(config('EMAIL'))
+password_field = browser_host.find_element_by_name("pass")
+password_field.send_keys(config('PASS'))
+password_field.send_keys(Keys.ENTER)
+facebook = browser_host.window_handles[0]
+##while True:
+##        try:
+##                Xpath1 = input("enter xpath1")
+##                browser_host.find_element_by_xpath(Xpath1).click()
+##                break
+##        except:
+##                print("can't press the button, try again")
+##while True:
+##        try:
+##                Xpath2 = input("enter xpath2")
+##                browser_host.find_element_by_xpath(Xpath2).click()
+##                break
+##        except:
+##                print("can't press the button, try again")
+while True:
+    try:
+        Xpath3 = input("enter Xpath3")
+        break
+    except:
+        print("can't press the button, try again")
+
+def set_up_facebook():
+##        browser_host.refresh
+##        browser_host.find_element_by_xpath(Xpath1).click()
+##        time.sleep(DELAY)
+##        browser_host.find_element_by_xpath(Xpath2).click()
+    time.sleep(DELAY)
+    browser_host.find_element_by_xpath(Xpath3).click()
+def get_coor():
+    set_up_facebook()
+    time.sleep(DELAY)
+    coor = browser_host.window_handles[1]
+
+    browser_host.switch_to.window(coor)
+    browser_host.find_element_by_xpath('//*[@id="maps_sb_container"]/div[1]/div[2]/a').click()
+    time.sleep(DELAY)
+    browser_host.find_element_by_xpath('//*[@id="maps_sb_container"]/div[1]/a/div/div/div').click()
+    time.sleep(DELAY)
+    latlong = browser_host.find_element_by_xpath('//*[@id="bm_overflowText"]').text
+    browser_host.close()
+    browser_host.switch_to.window(facebook)
+    return latlong
+
+def on_message(client, userdata, message):
+    latlong = get_coor()
+    client.publish("ku/daq2020/cosmic/suit", latlong + "," + str(message.payload.decode("utf-8"))+',"'+area_group+'"')
+
+mqttBroker ="iot.cpe.ku.ac.th"
+client = mqtt.Client("GPS")
+client.connect(mqttBroker) 
+
+client.loop_start()
+
+client.subscribe("ku/daq2020/cosmic/acc")
+client.on_message=on_message
+print(f"""Currently listening to {mqttBroker}/ku/daq2020/cosmic/acc for acceleration data
+After receiveing the data, it will be sent along with latitude and longtitude and area to {mqttBroker}/ku/daq2020/cosmic/suit""")
+
+
+
---
+++ Roughness measure.py
@@ -0,0 +1,59 @@
+import time
+import network
+from machine import Pin
+from umqtt.robust import MQTTClient
+import imu
+
+TIMES = 100
+SLEEP = 0.1 
+
+## imu set up
+acc = imu.acc
+save = [0,0,0]
+def diff(start, end):
+    re = [0,0,0]
+    for x in range (3):
+        re[x] = start[x] - end[x]
+    return re
+
+def copy(target):
+    re = [0,0,0]
+    for x in range (3):
+        re[x] = target[x]
+    return re
+
+def get_acc():
+    global acc, save
+    save = copy(acc)
+    imu.update()
+    return abs(diff(save, acc)[1])
+
+def get_avg_acc(times,sleep_time):
+    avg = 0
+    for itera in range(times):
+        avg += get_acc()
+        time.sleep(sleep_time)
+    return avg/times
+
+## connect wifi
+led_iot = Pin(12, Pin.OUT)
+led_wifi = Pin(2, Pin.OUT)
+led_wifi.value(1) # turn it off
+led_iot.value(1)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+print("*** Connecting to WiFi...")
+wlan.connect("NaviPhone","pjduck3qqvi43")
+while not wlan.isconnected():
+    time.sleep(0.5)
+print("*** Wifi connected")
+led_wifi.value(0)
+## connect broker
+mqtt = MQTTClient("Thananan ","iot.cpe.ku.ac.th")
+print("*** Connecting to MQTT broker...")
+mqtt.connect()
+print("*** MQTT broker connected")
+led_iot.value(0)
+
+while True:
+    mqtt.publish("ku/daq2020/cosmic/acc",str(get_avg_acc(TIMES,SLEEP)))
